c96fc25e9a5a962256a700831c649fda8721f254
Utilizador <Utilizador@WIN-MP3JADQD58A.local>
2020-10-30 12:20:00 +0000

    Update .editorconfig, .env.example, and 248 more files...

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,18 @@ Route::get('/', function () {
 });
 
 Route::get('login','App\Http\Controllers\FormController@loginForm')->name('login.form');
+
+Route::get('contactos','App\Http\Controllers\FormController@mostrarForm')->name('mostrar.form');
+
+Route::post('contactos','App\Http\Controllers\FormController@processarForm')->name('processar.form');
+Route::get('turbos','App\Http\Controllers\FormController@turbosForm')->name('turbos.form');
+
+Route::get('empresa','App\Http\Controllers\FormController@empresaForm')->name('empresa.form');
+
+Route::get('onde','App\Http\Controllers\FormController@ondeForm')->name('onde.form');
+
+Route::get('noticias','App\Http\Controllers\FormController@noticiasForm')->name('noticias.form');
+
+Route::get('welcome', function () {
+    return view('welcome');
+});
